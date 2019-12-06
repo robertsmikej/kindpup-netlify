e0024bbb5f68a10f2087899c5bd412bcff04cdf5
Michael <michael@robertsmj.com>
2019-12-06 09:23:57 -0700

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container">
-        <Hero :page="page"/>
+        <!-- <Hero :page="page"/> -->
         <!-- <Signup/>
         <SideBySide/> -->
         <!-- <Grid/> -->
